f2592d9147722b49ea006732e3c311909cf851cf
Bian Lee <bianlee2004@gmail.com>
2023-03-09 18:58:31 -0800

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,13 +35,14 @@ const BlogIndex = ({ data, location }) => {
                 itemScope
                 itemType="http://schema.org/Article"
               >
+                
                 <header>
                   <h2>
                     <Link to={post.fields.slug} itemProp="url">
                       <span itemProp="headline">{title}</span>
                     </Link>
                   </h2>
-                  <small>{post.frontmatter.date}</small>
+                  {/* <small>{post.frontmatter.date} </small> */}
                   <div style={{marginTop: "0.5rem"}}>
                   <p
                     dangerouslySetInnerHTML={{
